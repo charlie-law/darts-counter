a38ef4bf91f33a2bf58ffb097c683f82b825c8fa
cha-law <char_law@outlook.com>
2026-01-06 10:21:56 +0000

    started work on UI

---
+++ src/StartGame.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export default function StartGame() {
+    return (
+        <main>
+            <section>
+                <h1 className="text-5xl">Darts Counter</h1>
+                <p>By <a href="https://github.com/charlie-law">charlie-law</a></p>
+            </section>
+
+            <section>
+                <h2>Start a Game</h2>
+                <section>
+                    
+                </section>
+            </section>
+
+
+        </main>
+    )
+}
