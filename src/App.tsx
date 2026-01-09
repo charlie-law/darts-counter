77fcc4fac910f28a987a9d4bc204d767b58ebbe9
cha-law <char_law@outlook.com>
2026-01-09 11:04:46 +0000

    changed game variable to use useState, removed gameInfoObject and some style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Game from "./components/game";
 import Title from "./components/Title";
 import GameTypeRadio from "./components/GameTypeRadio";
@@ -7,25 +7,16 @@ import InputTypeRadio from "./components/InputTypeRadio";
 
 export default function App() {
     const [gameActive, setGameActive] = useState(false);
-    const [gameInfo, setGameInfo] = useState<{[index: string]: string}>({});
-    const game = useRef<Game>(new Game(301, "Player 1", "Player 2"));
+    const [game, setGame] = useState<Game>(new Game(301, "Player 1", "Player 2"));
+    const [round, setRound] = useState(0);
 
     function onSubmit(formData: FormData) {
         const gameType = String(document.querySelector('.game-type[aria-pressed="true"]')?.getAttribute("value")); // Get value of selected game type
         const player1Name = formData.get("player-1");
         const player2Name = formData.get("player-2");
 
-        game.current = new Game(Number(gameType), String(player1Name), String(player2Name));
-        console.log(game.current);
-
-        const gameInfoObject: {[index: string]: string} = {
-            "player1": String(player1Name),
-            "player2": String(player2Name),
-            "gameType": gameType,   
-            "player1Score": gameType,
-            "player2Score": gameType
-        };
-        setGameInfo(gameInfoObject);
+        setGame(new Game(Number(gameType), String(player1Name), String(player2Name)));
+        console.log(game);
 
         setGameActive(true); // Set game active to show game info page
     };
@@ -36,34 +27,37 @@ export default function App() {
                 <section className="flex flex-col gap-16">
                     <section className="flex gap-8">
                         <section className="flex flex-col gap-2 items-center flex-1">
-                            <h2 className="text-7xl font-bold">{gameInfo["player1Score"]}</h2>
-                            <p className="text-lg">{game.current.names[1]}</p>
+                            <h2 className="text-7xl font-bold">{game.scores[1]}</h2>
+                            <p className="text-lg">{game.names[1]}</p>
                         </section>
                         <div className="bg-gray-400 w-px"></div>
                         <section className="flex flex-col gap-2 items-center flex-1">
-                            <h2 className="text-7xl font-bold">{gameInfo["player2Score"]}</h2>
-                            <p className="text-lg">{game.current.names[2]}</p>
+                            <h2 className="text-7xl font-bold">{game.scores[2]}</h2>
+                            <p className="text-lg">{game.names[2]}</p>
                         </section>
                     </section>
                     <section className="flex flex-col gap-4 items-center">
                         <h3>CURRENT SCORE</h3>
                         <section className="flex gap-8 w-full">
-                            <section className="bg-gray-50 rounded border border-gray-200 p-2 flex-1">
-                                <h4 className="font-medium text-4xl">{game.current.currentScored[0]? game.current.currentScored[0].key : "-"}</h4>
-                                <p className="text-xl">{game.current.currentScored[0]? game.current.currentScored[0].value : 0}</p>
+                            <section className="bg-gray-50 rounded border border-gray-200 p-4 flex-1" id="current-points-1">
+                                <h4 className="font-semibold text-3xl">-</h4>
+                                <p className="text-xl">0</p>
                             </section>
-                            <section className="bg-gray-50 rounded border border-gray-200 p-2 flex-1">
-                                <h4 className="font-medium text-4xl">{game.current.currentScored[1]? game.current.currentScored[1].key : "-"}</h4>
-                                <p className="text-xl">{game.current.currentScored[1]? game.current.currentScored[1].value : 0}</p>
+                            <section className="bg-gray-50 rounded border border-gray-200 p-4 flex-1" id="current-points-2">
+                                <h4 className="font-semibold text-3xl">-</h4>
+                                <p className="text-xl">0</p>
                             </section>
-                            <section className="bg-gray-50 rounded border border-gray-200 p-2 flex-1">
-                                <h4 className="font-medium text-4xl">{game.current.currentScored[2]? game.current.currentScored[2].key : "-"}</h4>
-                                <p className="text-xl">{game.current.currentScored[2]? game.current.currentScored[2].value : 0}</p>
+                            <section className="bg-gray-50 rounded border border-gray-200 p-4 flex-1" id="current-points-3">
+                                <h4 className="font-semibold text-3xl">-</h4>
+                                <p className="text-xl">0</p>
                             </section>
                         </section>
-                        <button className="p-2 px-4 rounded bg-red-200 hover:bg-red-300 transition duration-250 w-fit cursor-pointer hidden">Confirm Score</button>
+                        <section className="flex gap-2">
+                            <button type="button" className="p-2 px-4 rounded bg-red-200 hover:bg-red-300 transition duration-250 w-fit cursor-pointer hidden confirmation-button" onClick={() => {game.changeScore(); setRound(round + 1);}}>Confirm Score</button>
+                            <button type="button" className="p-2 px-4 rounded bg-gray-100 hover:bg-gray-200 transition duration-250 w-fit cursor-pointer hidden back-button" onClick={() => {game.removeFromCurrentScored()}}>Back</button>
+                        </section>                    
                     </section>
-                    <InputTypeRadio game={game.current} />
+                    <InputTypeRadio game={game} />
                 </section>
             );
         } else {
