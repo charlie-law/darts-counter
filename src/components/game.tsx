84fc5a05870d845070aeec06284c14f37c62ee0f
cha-law <char_law@outlook.com>
2026-01-09 09:56:38 +0000

    added functions to game.tsx to configure the current scores and type annotations for the dictionaries

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,18 +1,19 @@
 export default class Game {
     playerStartScore: number; // The score that players start with
-    names = {
+    names: {[index: number]: string} = {
         1: "",
         2: ""
     };
-    scores = { // Current scores of the players
+    scores: {[index: number]: number} = { // Current scores of the players
         1: 0,
         2: 0
     };
-    legs = { // Total games won
+    legs: {[index: number]: number} = { // Total games won
         1: 0,
         2: 0
     }
     turn: number; // Current player's turn
+    currentScored: {[index: string]: number}[] = [];
 
     constructor(playerStartScore: number, player1Name: string, player2Name: string) {
         this.playerStartScore = playerStartScore;
@@ -21,5 +22,23 @@ export default class Game {
         this.scores[1] = playerStartScore;
         this.scores[2] = playerStartScore;
         this.turn = 1;
+    };
+    
+    addToCurrentScored(point: {[index: string]: number}) {
+        this.currentScored.push(point);
+        if (this.currentScored.length == 3) {
+            // Show confirmation button
+            console.log(this.currentScored)
+        };
+    };
+
+    changeScore(pointsArray: number[]) {
+        let totalPoints = 0;
+        for (let i in pointsArray) {
+            totalPoints += pointsArray[i];
+        };
+
+        this.scores[this.turn] -= totalPoints;
+        this.turn == 1? this.turn = 0 : this.turn = 1;
     };
 };
