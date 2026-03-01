6a31cb0cc6c43294350b7097dee52a11c9c4abd6
charlie-law <char_law@outlook.com>
2026-03-01 21:04:55 +0000

    feat: added Inter font

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title() {
     return (
         <section>
-            <h1 className="text-6xl">🎯 Darts Counter</h1>
+            <h1 className="text-6xl font-semibold">🎯 Darts Counter</h1>
             <p>By <a className="text-red-700 hover:text-red-900" href="https://github.com/charlie-law">charlie-law</a></p>
         </section>
     )
